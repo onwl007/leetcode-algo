33406d13623fb7b3e772a6ecfb866ab1b75fe8db
onwl007 <onwl007@126.com>
2020-10-20 22:00:14 +0800

    无重复字符的最长子串 第三次尝试 失败

--- leetcode-go/003.Longest-Substring-Without-Repeat-Charcters/longest_substring_without_repeat_charcters.go
+++ leetcode-go/003.Longest-Substring-Without-Repeat-Charcters/longest_substring_without_repeat_charcters.go
@@ -1,23 +1,29 @@
-package leetcode
+package main
+
+import "fmt"
+
+func main() {
+	lengthOfLongestSubstring("abcabcbb")
+}
 
 func lengthOfLongestSubstring(s string) int {
-	s = "abcabcbb"
-	m := make(map[byte]int)
+	fmt.Println(s)
 	length := len(s)
+	i, j := 0, 0
 	maxlen := 0 // 存储当前窗口最长子串的长度
-	for i := 0; i < length; i++ {
-		for j := i; j < length; j++ {
-			if _, ok := m[s[j]]; ok {
+	for i = 0; i < length; i++ {
+		for j = i + 1; j < length; j++ {
+			fmt.Println(string(s[i]))
+			if s[i] == s[j] {
+				i++
 				break
 			} else {
-				m[s[j]] = j
+				maxlen = j - i + 1
 			}
 		}
-		maxlen = max(maxlen, len(m))
-		if maxlen >= length-1-i {
-			break
-		}
+		maxlen = max(maxlen, j-i+1)
 	}
+	fmt.Println("max", maxlen)
 	return maxlen
 }
 
