550e16e449cf08174721f0010f2507efa037be3a
onwl007 <onwl007@126.com>
2020-11-08 22:22:11 +0800

    删除排序数组的重复项

--- leetcode-go/026.Remove-Duplicates-from-Sorted-Array/remove_duplicates.go
+++ leetcode-go/026.Remove-Duplicates-from-Sorted-Array/remove_duplicates.go
@@ -1,16 +1,17 @@
 package main
 
 func removeDuplicates(nums []int) int {
-	if len(nums) == 0 {
-		return len(nums)
+	n := len(nums)
+	if n == 0 {
+		return 0
 	}
+	// 通过双指针比较法，将数组前面的元素都赋值成不相同的，只需要返回 i 指针的下一位即可
 	i := 0
-	for j := 1; j < len(nums); j++ {
+	for j := 1; j < n; j++ {
 		if nums[i] != nums[j] {
 			i++
 			nums[i] = nums[j]
 		}
 	}
-	nums = nums[:i+1]
-	return len(nums)
+	return i + 1
 }
