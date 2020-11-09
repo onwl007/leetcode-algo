36e5d292a42fe52fe4ef94ff50f5f47ba5b711d3
onwl007 <onwl007@126.com>
2020-11-09 21:41:57 +0800

    移除元素 双指针法 第二种方法

--- leetcode-go/Remove-Element/remove_element.go
+++ leetcode-go/Remove-Element/remove_element.go
@@ -10,3 +10,19 @@ func removeElement(nums []int, val int) int {
 	}
 	return i
 }
+
+// 当数组包含很少要删除的元素的情况，方法二可以减少对多余的数组元素复制操作
+func removeElement1(nums []int, val int) int {
+	i := 0
+	n := len(nums)
+	for i < n {
+		// 当前元素与 val 相等时，将最后一个元素的值复制到当前位置
+		if nums[i] == val {
+			nums[i] = nums[n-1]
+			n--
+		} else {
+			i++
+		}
+	}
+	return n
+}
