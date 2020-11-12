449aae0e93afffd8241ba3c0a9bbe5baf6730136
onwl007 <onwl007@126.com>
2020-11-12 21:47:53 +0800

    搜索插入位置 二分查找

--- leetcode-go/035.Search-Insert-Position/search-insert-position.go
+++ leetcode-go/035.Search-Insert-Position/search-insert-position.go
@@ -1,13 +1,16 @@
 package main
 
 func searchInsert(nums []int, target int) int {
-	x := 0
-	for i := 0; i < len(nums); i++ {
-		if target > nums[i] {
-			x++
-		} else {
-			return i
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if target == nums[mid] {
+			return mid
+		} else if target < nums[mid] {
+			right = mid - 1
+		} else if target > nums[mid] {
+			left = mid + 1
 		}
 	}
-	return x
+	return left
 }
