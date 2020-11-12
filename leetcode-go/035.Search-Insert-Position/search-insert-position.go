3fd99645ce0ff53f7c3ab93bfd72f00bf22ae330
onwl007 <onwl007@126.com>
2020-11-12 22:43:05 +0800

    搜索插入位置

--- leetcode-go/035.Search-Insert-Position/search-insert-position.go
+++ leetcode-go/035.Search-Insert-Position/search-insert-position.go
@@ -14,3 +14,15 @@ func searchInsert(nums []int, target int) int {
 	}
 	return left
 }
+
+func searchInsert1(nums []int, target int) int {
+	x := 0
+	for i := 0; i < len(nums); i++ {
+		if target > nums[i] {
+			x++
+		} else {
+			return i
+		}
+	}
+	return x
+}
