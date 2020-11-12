1b41ed0404faa4425f1ca33860c7c963cf7cc817
onwl007 <onwl007@126.com>
2020-11-12 21:44:08 +0800

    搜索插入位置 第一次想到的解法 第一优化

--- leetcode-go/035.Search-Insert-Position/search-insert-position.go
+++ leetcode-go/035.Search-Insert-Position/search-insert-position.go
@@ -3,12 +3,10 @@ package main
 func searchInsert(nums []int, target int) int {
 	x := 0
 	for i := 0; i < len(nums); i++ {
-		if target == nums[i] {
-			return i
-		} else if target < nums[i] {
-			return i
-		} else if target > nums[i] {
+		if target > nums[i] {
 			x++
+		} else {
+			return i
 		}
 	}
 	return x
