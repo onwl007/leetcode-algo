01dea53543d83bbf1642be2e0776429d70bc9cba
onwl007 <onwl007@126.com>
2020-11-12 21:56:20 +0800

    搜索插入位置 单元测试

---
+++ leetcode-go/035.Search-Insert-Position/search-insert-position_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+type question35 struct {
+	para35
+	ans35
+}
+
+type para35 struct {
+	nums   []int
+	target int
+}
+
+type ans35 struct {
+	one int
+}
+
+func Test_Problem35(t *testing.T) {
+	qs := []question35{
+
+		{
+			para35{[]int{1, 3, 5, 6}, 5},
+			ans35{2},
+		},
+
+		{
+			para35{[]int{1, 3, 5, 6}, 2},
+			ans35{1},
+		},
+
+		{
+			para35{[]int{1, 3, 5, 6}, 7},
+			ans35{4},
+		},
+
+		{
+			para35{[]int{1, 3, 5, 6}, 0},
+			ans35{0},
+		},
+	}
+
+	fmt.Printf("------------------------Leetcode Problem 35------------------------\n")
+
+	for _, q := range qs {
+		_, p := q.ans35, q.para35
+		fmt.Printf("[intput]:%v      [output]:%v\n", p, searchInsert(p.nums, p.target))
+	}
+	fmt.Printf("\n\n\n")
+}
