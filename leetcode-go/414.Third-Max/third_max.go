99b2c0f0e340636ae56ff10733241bb874dee9cc
onwl007 <onwl007@126.com>
2020-11-24 22:01:27 +0800

    414 第三大的数 时间复杂度 O(n) 解法

--- leetcode-go/414.Third-Max/third_max.go
+++ leetcode-go/414.Third-Max/third_max.go
@@ -1,10 +1,35 @@
 package main
 
-import "sort"
+import (
+	"math"
+	"sort"
+)
+
+// 动态维护 3 个最大值即可，注意数组有重复数据的情况。
+// 如果只有 2 个数或者 1 个数，则返回最大值即可。
+func thirdMax(nums []int) int {
+	one, two, three := math.MinInt64, math.MinInt64, math.MinInt64
+	for _, v := range nums {
+		if v > one {
+			three = two
+			two = one
+			one = v
+		} else if v < one && v > two {
+			three = two
+			two = v
+		} else if v < two && v > three {
+			three = v
+		}
+	}
+	if three == math.MinInt64 {
+		return one
+	}
+	return three
+}
 
 // 时间复杂度太高
 // 去重
-// 排序
+// 排序，时间复杂度不是 O(n)，所以不符合题意
 // 步骤太多
 func thirdMax1(nums []int) int {
 	nums = removeDuplicate(nums)
