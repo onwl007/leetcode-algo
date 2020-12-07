3d72ae5480a23ed4b419f17c34781c99dbc14425
onwl007 <onwl007@126.com>
2020-12-07 22:24:20 +0800

    144 二叉树的前序遍历

---
+++ leetcode-go/144.Binary-Tree-Preorder-Traversal/preorder_traversal.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/onwl007/leetcode-algo/structures"
+
+type TreeNode = structures.TreeNode
+
+func preorderTraversal(root *TreeNode) (vals []int) {
+	var preorder func(*TreeNode)
+	preorder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		vals = append(vals, node.Val)
+		preorder(node.Left)
+		preorder(node.Right)
+	}
+	preorder(root)
+	return
+}
