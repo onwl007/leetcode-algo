9a90de66581c6483daa35ff67f97f5472f56482a
onwl007 <onwl007@126.com>
2020-12-09 23:17:22 +0800

    100 相同的树 递归解法

---
+++ leetcode-go/100.Binary-Tree-Same/binary_tree_same.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/onwl007/leetcode-algo/structures"
+
+type TreeNode = structures.TreeNode
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+
+	}
+	if p.Val != q.Val {
+		return false
+	}
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
