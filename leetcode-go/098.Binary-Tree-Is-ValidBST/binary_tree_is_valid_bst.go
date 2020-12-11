1ad4ecb27dcd7fa4bb72bf749c0b94d2534b2e46
onwl007 <onwl007@126.com>
2020-12-11 22:48:24 +0800

    98 验证二叉搜索树 添加注释

--- leetcode-go/098.Binary-Tree-Is-ValidBST/binary_tree_is_valid_bst.go
+++ leetcode-go/098.Binary-Tree-Is-ValidBST/binary_tree_is_valid_bst.go
@@ -15,9 +15,11 @@ func isBST(root *TreeNode, min *TreeNode, max *TreeNode) bool {
 	if root == nil {
 		return true
 	}
+	// 判断右子树的值小于其根节点的值，不符合 二叉搜索树
 	if min != nil && root.Val <= min.Val {
 		return false
 	}
+	// 判断左子树的节点大于其根节点的值，不符合二叉搜索树
 	if max != nil && root.Val >= max.Val {
 		return false
 	}
