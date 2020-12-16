a52dea88fc28065a1fca804892df5e57da13ecba
onwl007 <onwl007@126.com>
2020-12-16 19:14:38 +0800

    增加将数组转成二叉树

--- structures/TreeNode.go
+++ structures/TreeNode.go
@@ -5,3 +5,38 @@ type TreeNode struct {
 	Left  *TreeNode
 	Right *TreeNode
 }
+
+var NULL = -1 << 63
+
+// 将数组转成二叉树
+func Ints2TreeNode(arr []int) *TreeNode {
+	n := len(arr)
+	if n == 0 {
+		return nil
+	}
+
+	root := &TreeNode{Val: arr[0]}
+
+	queue := make([]*TreeNode, 1, n*2)
+	queue[0] = root
+
+	i := 1
+	for i < n {
+		node := queue[0]
+		queue = queue[1:]
+
+		if i < n && arr[i] != NULL {
+			node.Left = &TreeNode{Val: arr[i]}
+			queue = append(queue, node.Left)
+		}
+		i++
+
+		if i < n && arr[i] != NULL {
+			node.Right = &TreeNode{Val: arr[i]}
+			queue = append(queue, node.Right)
+		}
+		i++
+	}
+
+	return root
+}
