dc862bf0b8fe6929ba49e50d3c596a0d15ad1c6a
onwl007 <onwl007@126.com>
2020-12-16 23:37:32 +0800

    104 二叉树最大深度 BFS 遍历

--- leetcode-go/104.Binary-Tree-Max-Depth/binary_tree_max_depth.go
+++ leetcode-go/104.Binary-Tree-Max-Depth/binary_tree_max_depth.go
@@ -19,3 +19,26 @@ func max(a, b int) int {
 	}
 	return b
 }
+
+func maxDepthBFS(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	queue := []*TreeNode{root}
+	depth := 0
+	for len(queue) != 0 {
+		size := len(queue)
+		for i := 0; i < size; i++ {
+			cur := queue[0]   // 取队头元素
+			queue = queue[1:] // 队头元素出队
+			if cur.Left != nil {
+				queue = append(queue, cur.Left)
+			}
+			if cur.Right != nil {
+				queue = append(queue, cur.Right)
+			}
+		}
+		depth++
+	}
+	return depth
+}
