b243b2f359caeaddad8497e9f92ac083b1b99be0
onwl007 <onwl007@126.com>
2020-12-23 20:02:36 +0800

    116 填充每个节点的下一个右侧节点指针 增加注释

--- leetcode-go/102.Binary-Tree-Level-Order/main.go
+++ leetcode-go/102.Binary-Tree-Level-Order/main.go
@@ -8,8 +8,9 @@ import (
 
 func main() {
 	arr := []int{3, 9, 20, structures.NULL, structures.NULL, 15, 7}
+	arr1 := []int{1, 2, 3, 4, 5, 6, 7}
 	// 一维数组返回
-	vals1 := levelOrder1(structures.Ints2TreeNode(arr))
+	vals1 := levelOrder1(structures.Ints2TreeNode(arr1))
 	fmt.Println(vals1)
 	fmt.Println("-----------")
 	// 二维数组返回
--- leetcode-go/116.Populating-Next-Right-Pointers-in-Each-Node/populating_next_right_pointers_in_each_node.go
+++ leetcode-go/116.Populating-Next-Right-Pointers-in-Each-Node/populating_next_right_pointers_in_each_node.go
@@ -7,13 +7,17 @@ import (
 type Node = structures.Node
 
 func connect(root *Node) *Node {
+	// Time: O(N) Space: O(N)
 	if root == nil {
 		return nil
 	}
+	// 初始化队列，将根节点加入队列
 	queue := []*Node{root}
+	// 循环迭代层数，而不是每个节点
 	for len(queue) != 0 {
 		tmp := queue
 		queue = nil
+		// 遍历这一层的节点
 		for i, node := range tmp {
 			if i+1 < len(tmp) {
 				node.Next = tmp[i+1]
