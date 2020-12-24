2c584d09580fd425768de3a65686447e027bfc86
onwl007 <onwl007@126.com>
2020-12-25 00:08:58 +0800

    235 二叉搜索树的最近公共祖先

---
+++ leetcode-go/235.Lowest-Common-Ancestor-of-a-Binary-Search-Tree/lowest_common_ancestor_of_a_binary_search_bst.go
@@ -0,0 +1,32 @@
+package main
+
+import "github.com/onwl007/leetcode-algo/structures"
+
+type TreeNode = structures.TreeNode
+
+// 递归解法
+// 二叉搜索树，根节点大于左子树所有的值，小于右子树所有的值
+// 当 p，q 同时小于根节点，递归左子树
+// 当 p，q 同时大于根节点，递归右子树
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if p.Val < root.Val && q.Val < root.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	}
+	if p.Val > root.Val && q.Val > root.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	}
+	return root
+}
+
+func lowestCommonAncestor1(root, p, q *TreeNode) *TreeNode {
+	for root != nil {
+		if p.Val < root.Val && q.Val < root.Val {
+			root = root.Left
+		} else if p.Val < root.Val && q.Val < root.Val {
+			root = root.Right
+		} else {
+			break
+		}
+	}
+	return root
+}
