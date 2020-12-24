3e708ea9327062c7ca857f24366d346c97b19edd
onwl007 <onwl007@126.com>
2020-12-24 22:21:05 +0800

    236 二叉树的最近公共祖先

--- leetcode-go/236.Lowest-Common-Ancestor-of-a-Binary-Tree/lowest_common_ancestor_of_a_binary_tree.go
+++ leetcode-go/236.Lowest-Common-Ancestor-of-a-Binary-Tree/lowest_common_ancestor_of_a_binary_tree.go
@@ -8,19 +8,16 @@ func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
 	if root == nil {
 		return nil
 	}
-	if p == root || q == root {
-		return nil
+	if root.Val == p.Val || root.Val == q.Val {
+		return root
 	}
 	left := lowestCommonAncestor(root.Left, p, q)
 	right := lowestCommonAncestor(root.Right, p, q)
 	if left != nil && right != nil {
 		return root
 	}
-	if left == nil && right == nil {
-		return nil
+	if left != nil {
+		return left
 	}
-	if left == nil {
-		return right
-	}
-	return left
+	return right
 }
