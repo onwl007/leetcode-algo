43512ebdf1068f7ae2af00ac877120ab7805ec0f
onwl007 <onwl007@126.com>
2021-01-03 21:22:58 +0800

    237 删除链表中的节点

---
+++ leetcode-go/237. Delete Node in a Linked List/delete_node_in_a_linked_list.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/onwl007/leetcode-algo/structures"
+
+type ListNode = structures.ListNode
+
+// 这道题其实不是直接删除节点 node
+// 而是用 node 的下一个节点覆盖 node 节点
+// 真正删除的其实是 node 的下一个节点
+func deleteNode(node *ListNode) {
+	node.Val = node.Next.Val
+	node.Next = node.Next.Next
+}
