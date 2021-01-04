13996f80a6782d0292f2775c125ecba2ee8d3026
onwl007 <onwl007@126.com>
2021-01-04 22:12:57 +0800

    141 环形链表 哈希表解法

--- leetcode-go/141. Linked List Cycle/linked_list_cycle.go
+++ leetcode-go/141. Linked List Cycle/linked_list_cycle.go
@@ -4,6 +4,7 @@ import "github.com/onwl007/leetcode-algo/structures"
 
 type ListNode = structures.ListNode
 
+// 快慢指针
 func hasCycle(head *ListNode) bool {
 	if head == nil || head.Next == nil {
 		return false
@@ -18,4 +19,17 @@ func hasCycle(head *ListNode) bool {
 		fast = fast.Next.Next
 	}
 	return true
+}
+
+// 哈希表
+func hasCycle1(head *ListNode) bool {
+	m := make(map[*ListNode]struct{})
+	for head != nil {
+		if _, ok := m[head]; ok {
+			return true
+		}
+		m[head] = struct{}{}
+		head = head.Next
+	}
+	return false
 }
