4af61f13b94c7180478151819d30ad9d039b643c
onwl007 <onwl007@126.com>
2021-01-05 23:02:18 +0800

    142 环形链表 快慢指针解法

--- leetcode-go/142. Linked List Cycle II/linked_list_cycle_II.go
+++ leetcode-go/142. Linked List Cycle II/linked_list_cycle_II.go
@@ -34,3 +34,25 @@ func detectCycle1(head *ListNode) *ListNode {
 	}
 	return nil
 }
+
+func detectCycle2(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if slow == fast {
+			break
+		}
+	}
+
+	if fast == nil || fast.Next == nil {
+		return nil
+	}
+
+	slow = head
+	for slow != fast {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	return slow
+}
