6b017b5b1d89caab09ed507c9ffe3fdd280e628b
onwl007 <onwl007@126.com>
2021-01-06 09:56:57 +0800

    生成随机题目

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	fileNames := []string{}
+	files, _ := ioutil.ReadDir("./leetcode-go")
+	for _, f := range files {
+		fileNames = append(fileNames, f.Name())
+	}
+	n := getRandNumber(len(fileNames))
+	fmt.Printf("生成的随机题目是 ----- %v\n", fileNames[n])
+}
+
+func getRandNumber(n int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(n)
+}
