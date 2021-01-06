4be5fd556cbcfa52047d2697902815aa0108476a
onwl007 <onwl007@126.com>
2021-01-06 23:19:51 +0800

    82 删除排序链表中的重复元素 II

---
+++ leetcode-go/82. Remove Duplicates from Sorted List II/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/onwl007/leetcode-algo/structures"
+
+func main() {
+	arr := []int{1, 2, 3, 3, 4, 4, 5}
+	deleteDuplicates(structures.Ints2ListNode(arr))
+}
---
+++ leetcode-go/82. Remove Duplicates from Sorted List II/remove_duplicates_from_sorted_list_II.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"sort"
+
+	"github.com/onwl007/leetcode-algo/structures"
+)
+
+type ListNode = structures.ListNode
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	m := make(map[int]int)
+	for head != nil {
+		if _, ok := m[head.Val]; ok {
+			m[head.Val]++
+		} else {
+			m[head.Val] = 1
+		}
+	}
+
+	vals := []int{}
+	for _, v := range m {
+		if v == 1 {
+			vals = append(vals, v)
+		}
+	}
+	sort.Ints(vals)
+
+	dummy := &ListNode{}
+	pre := dummy
+	for _, v := range vals {
+		tmp := &ListNode{Val: v}
+		pre.Next = tmp
+		pre = pre.Next
+	}
+	return dummy.Next
+}
