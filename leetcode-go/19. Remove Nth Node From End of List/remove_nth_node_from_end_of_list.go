4642ec24d88d6e873309c6c08539b2eff7141839
onwl007 <onwl007@126.com>
2021-01-07 23:25:30 +0800

    19 删除链表的倒数第N个节点 快慢指针解法

--- leetcode-go/19. Remove Nth Node From End of List/remove_nth_node_from_end_of_list.go
+++ leetcode-go/19. Remove Nth Node From End of List/remove_nth_node_from_end_of_list.go
@@ -51,3 +51,39 @@ func getLength(head *ListNode) (length int) {
 	}
 	return
 }
+
+// 可以通过栈的这种形式，遍历的时候一次入栈
+// 我们弹出栈的第 nn 个节点就是需要删除的节点，
+// 并且目前栈顶的节点就是待删除节点的前驱节点
+func removeNthFromEnd2(head *ListNode, n int) *ListNode {
+	vals := []*ListNode{}
+	dummy := &ListNode{Val: 0, Next: head}
+	for node := dummy; node != nil; node = node.Next {
+		vals = append(vals, node)
+	}
+	pre := vals[len(vals)-n-1]
+	pre.Next = pre.Next.Next
+	return dummy.Next
+}
+
+// 快慢指针
+// 快慢指针同时指向 head 结点时，快指针先移动 n 次，
+// 然后快慢指针同时移动，当快指针移动到链表尾部时，慢指针此时正好移动到倒数第 n 个结点
+// 本题需要删除倒数第 n 个节点，我们把慢指针指向 dummy 哑结点，其余步骤不变，
+// 此时快指针移动链表尾部，慢指针正好移动到倒数第 n 个节点的前驱结点
+// 我们正好把前驱结点指向其后继结点，这样就实现了删除倒数第 n 个节点
+func removeNthFromEnd3(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{Val: 0, Next: head}
+	slow, fast := dummy, head
+	for n > 0 {
+		fast = fast.Next
+		n--
+	}
+
+	for fast != nil {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	slow.Next = slow.Next.Next
+	return dummy.Next
+}
