f30783f17b22bcb9736795a135a83acdb97f5ac1
onwl007 <onwl007@126.com>
2021-01-09 15:02:07 +0800

    136 只出现一次的数字 异或解法

--- leetcode-go/136. Single Number/single_number.go
+++ leetcode-go/136. Single Number/single_number.go
@@ -1,7 +1,16 @@
 package main
 
-// 两次循环，解法不是很好
+// 异或解法
 func singleNumber(nums []int) int {
+	single := 0
+	for i := 0; i < len(nums); i++ {
+		single ^= nums[i]
+	}
+	return single
+}
+
+// 两次循环，解法不是很好
+func singleNumber1(nums []int) int {
 	m := make(map[int]int)
 	for i := 0; i < len(nums); i++ {
 		if _, ok := m[nums[i]]; ok {
@@ -21,7 +30,7 @@ func singleNumber(nums []int) int {
 }
 
 // 双重循环，暴力法解决
-func singleNumber1(nums []int) int {
+func singleNumber2(nums []int) int {
 	for i := 0; i < len(nums); i++ {
 		count := 0
 		for j := 0; j < len(nums); j++ {
