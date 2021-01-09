720138c209bf28931ac6ba3f4da944a60261e935
onwl007 <onwl007@126.com>
2021-01-09 14:18:56 +0800

    136 只出现一次的数字

---
+++ leetcode-go/136. Single Number/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	res := singleNumber1([]int{2, 2, 1})
+	fmt.Println(res)
+}
---
+++ leetcode-go/136. Single Number/single_number.go
@@ -0,0 +1,37 @@
+package main
+
+// 两次循环，解法不是很好
+func singleNumber(nums []int) int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		if _, ok := m[nums[i]]; ok {
+			m[nums[i]]++
+		} else {
+			m[nums[i]] = 1
+		}
+	}
+
+	for k, v := range m {
+		if v != 1 {
+			continue
+		}
+		return k
+	}
+	return 0
+}
+
+// 双重循环，暴力法解决
+func singleNumber1(nums []int) int {
+	for i := 0; i < len(nums); i++ {
+		count := 0
+		for j := 0; j < len(nums); j++ {
+			if nums[i] == nums[j] {
+				count++
+			}
+		}
+		if count == 1 {
+			return nums[i]
+		}
+	}
+	return 0
+}
