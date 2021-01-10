9c53645a3237ae64e672d763217e116fdde5c458
onwl007 <onwl007@126.com>
2021-01-10 18:47:26 +0800

    217 存在重复元素 哈希表和排序解法

---
+++ leetcode-go/217. Contains Duplicate/contains_duplicate.go
@@ -0,0 +1,26 @@
+package main
+
+import "sort"
+
+// 哈希表
+func containsDuplicate(nums []int) bool {
+	m := make(map[int]int)
+	for i, v := range nums {
+		m[v] = i
+	}
+
+	return len(nums) != len(m)
+}
+
+// 排序
+// 排完序后，相等的元素一定相邻
+// 注意数组越界
+func containsDuplicate1(nums []int) bool {
+	sort.Ints(nums)
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] == nums[i+1] {
+			return true
+		}
+	}
+	return false
+}
