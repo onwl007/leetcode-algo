2dbf5257a5e2b196d5c40d4e860bfb8035c61d56
onwl007 <onwl007@126.com>
2021-01-14 15:44:15 +0800

    1290 二进制链表转整数

--- leetcode-go/1290. Convert Binary Number in a Linked List to Integer/convert_binary_number_in_a_inked_list_to_integer.go
+++ leetcode-go/1290. Convert Binary Number in a Linked List to Integer/convert_binary_number_in_a_inked_list_to_integer.go
@@ -4,11 +4,27 @@ import "github.com/onwl007/leetcode-algo/structures"
 
 type ListNode = structures.ListNode
 
+/*
+ * @lc app=leetcode.cn id=1290 lang=golang
+ *
+ * [1290] 二进制链表转整数
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
 func getDecimalValue(head *ListNode) int {
 	sum := 0
 	for head != nil {
-		sum = (sum << 1) + head.Val
+		sum = sum<<1 + head.Val
 		head = head.Next
 	}
 	return sum
 }
+
+// @lc code=end
