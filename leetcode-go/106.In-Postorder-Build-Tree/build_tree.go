26cbedab9b1f9a485569778961228e13cea0cba3
onwl007 <onwl007@126.com>
2021-01-15 14:46:24 +0800

    106 中序遍历和后序遍历构造二叉树

--- leetcode-go/106.In-Postorder-Build-Tree/build_tree.go
+++ leetcode-go/106.In-Postorder-Build-Tree/build_tree.go
@@ -4,6 +4,21 @@ import "github.com/onwl007/leetcode-algo/structures"
 
 type TreeNode = structures.TreeNode
 
+/*
+ * @lc app=leetcode.cn id=106 lang=golang
+ *
+ * [106] 从中序与后序遍历序列构造二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
 func buildTree(inorder []int, postorder []int) *TreeNode {
 	if len(inorder) == 0 || len(postorder) == 0 {
 		return nil
@@ -20,3 +35,5 @@ func buildTree(inorder []int, postorder []int) *TreeNode {
 	root.Right = buildTree(inorder[i+1:], postorder[i:len(postorder)-1])
 	return root
 }
+
+// @lc code=end
--- leetcode-go/237. Delete Node in a Linked List/delete_node_in_a_linked_list.go
+++ leetcode-go/237. Delete Node in a Linked List/delete_node_in_a_linked_list.go
@@ -7,7 +7,24 @@ type ListNode = structures.ListNode
 // 这道题其实不是直接删除节点 node
 // 而是用 node 的下一个节点覆盖 node 节点
 // 真正删除的其实是 node 的下一个节点
+
+/*
+ * @lc app=leetcode.cn id=237 lang=golang
+ *
+ * [237] 删除链表中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
 func deleteNode(node *ListNode) {
 	node.Val = node.Next.Val
 	node.Next = node.Next.Next
 }
+
+// @lc code=end
