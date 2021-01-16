3ddd14c22fcd39e3d595e382a0b0c142ce117505
onwl007 <onwl007@126.com>
2021-01-16 16:59:05 +0800

    560 和为k的子数组 枚举解法

---
+++ leetcode-go/560. Subarray Sum Equals K/subarray_sum_equals_k.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	res := subarraySum([]int{1, 1, 1}, 2)
+	fmt.Println(res)
+}
+
+/*
+ * @lc app=leetcode.cn id=560 lang=golang
+ *
+ * [560] 和为K的子数组
+ */
+
+// @lc code=start
+func subarraySum(nums []int, k int) int {
+	count := 0
+	for i := 0; i < len(nums); i++ {
+		sum := 0
+		for j := i; j < len(nums); j++ {
+			sum += nums[j]
+			if sum == k {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+// @lc code=end
