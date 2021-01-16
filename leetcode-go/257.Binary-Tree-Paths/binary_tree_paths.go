80eef8cc454663fc4a8b13e1758877ea51d7f780
onwl007 <onwl007@126.com>
2021-01-16 10:41:07 +0800

    257 二叉树的所有路径 题解

--- leetcode-go/257.Binary-Tree-Paths/binary_tree_paths.go
+++ leetcode-go/257.Binary-Tree-Paths/binary_tree_paths.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/onwl007/leetcode-algo/structures"
 )
 
+func main() {
+	arr := []int{1, 2, 3, structures.NULL, 5}
+	res := binaryTreePaths(structures.Ints2TreeNode(arr))
+	fmt.Println(res)
+}
+
 type TreeNode = structures.TreeNode
 
+/*
+ * @lc app=leetcode.cn id=257 lang=golang
+ *
+ * [257] 二叉树的所有路径
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
 func binaryTreePaths(root *TreeNode) []string {
-	vals := []string{}
-	res := ""
+	var vals []string
+	var res string
 	dfs(root, &vals, res)
 	return vals
 }
@@ -28,3 +50,5 @@ func dfs(root *TreeNode, vals *[]string, res string) {
 	dfs(root.Left, vals, res)
 	dfs(root.Right, vals, res)
 }
+
+// @lc code=end
