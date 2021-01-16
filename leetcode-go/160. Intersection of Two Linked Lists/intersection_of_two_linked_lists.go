fbeaf96ba4504a437c9d47c428fc2504a4ab7f64
onwl007 <onwl007@126.com>
2021-01-16 11:15:16 +0800

    160 相交链表

--- leetcode-go/160. Intersection of Two Linked Lists/intersection_of_two_linked_lists.go
+++ leetcode-go/160. Intersection of Two Linked Lists/intersection_of_two_linked_lists.go
@@ -4,23 +4,39 @@ import "github.com/onwl007/leetcode-algo/structures"
 
 type ListNode = structures.ListNode
 
-// map 法
+// map
+/*
+ * @lc app=leetcode.cn id=160 lang=golang
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
-	m := make(map[interface{}]interface{})
+	m := make(map[*ListNode]int)
 
 	for headA != nil {
-		m[headA] = 1
+		m[headA]++
 		headA = headA.Next
 	}
 
 	for headB != nil {
-		if m[headB] == 1 {
+		if _, ok := m[headB]; ok {
 			return headB
 		}
 		headB = headB.Next
 	}
 	return nil
 }
+
+// @lc code=end
 
 // 暴力法
 func getIntersectionNode1(headA, headB *ListNode) *ListNode {
