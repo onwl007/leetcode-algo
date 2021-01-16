41266e5418e356ec9e2552e8eb4d22625da4eacf
onwl007 <onwl007@126.com>
2021-01-16 11:03:39 +0800

    104 返回二叉树的最大深度 题解

--- leetcode-go/104.Binary-Tree-Max-Depth/binary_tree_max_depth.go
+++ leetcode-go/104.Binary-Tree-Max-Depth/binary_tree_max_depth.go
@@ -1,10 +1,35 @@
 package main
 
-import "github.com/onwl007/leetcode-algo/structures"
+import (
+	"fmt"
+
+	"github.com/onwl007/leetcode-algo/structures"
+)
+
+func main() {
+	arr := []int{3, 9, 20, structures.NULL, structures.NULL, 15, 7}
+	depth := maxDepthBFS(structures.Ints2TreeNode(arr))
+	fmt.Println(depth)
+}
 
 type TreeNode = structures.TreeNode
 
 // 只需要求左子树和右子树的最大深度 l 和 r，那么该二叉树的最大深度即为 max(l,r) + 1
+/*
+ * @lc app=leetcode.cn id=104 lang=golang
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
 func maxDepth(root *TreeNode) int {
 	// 递归终止条件
 	if root == nil {
@@ -19,6 +44,8 @@ func max(a, b int) int {
 	}
 	return b
 }
+
+// @lc code=end
 
 func maxDepthBFS(root *TreeNode) int {
 	if root == nil {
