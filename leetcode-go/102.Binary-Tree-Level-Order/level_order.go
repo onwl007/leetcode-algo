9bee35487b895892857fc19eab4f23dd06bb5d2b
onwl007 <onwl007@126.com>
2021-01-27 22:57:13 +0800

    102 二叉树的层序遍历 增加单元测试

--- leetcode-go/102.Binary-Tree-Level-Order/level_order.go
+++ leetcode-go/102.Binary-Tree-Level-Order/level_order.go
@@ -1,12 +1,70 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/onwl007/leetcode-algo/structures"
 )
 
 type TreeNode = structures.TreeNode
 
+func main() {
+	arr := []int{3, 9, 20, structures.NULL, structures.NULL, 15, 7}
+	arr1 := []int{1, 2, 3, 4, 5, 6, 7}
+	// 一维数组返回
+	vals1 := levelOrder1(structures.Ints2TreeNode(arr1))
+	fmt.Println(vals1)
+	fmt.Println("-----------")
+	// 二维数组返回
+	vals2 := levelOrder(structures.Ints2TreeNode(arr))
+	fmt.Println(vals2)
+}
+
+/*
+ * @lc app=leetcode.cn id=102 lang=golang
+ *
+ * [102] 二叉树的层序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
 func levelOrder(root *TreeNode) [][]int {
+	vals := [][]int{}
+	if root == nil {
+		return vals
+	}
+
+	queue := []*TreeNode{root}
+	for len(queue) != 0 {
+		size := len(queue)
+		res := []int{}
+		for i := 0; i < size; i++ {
+			node := queue[i]
+			res = append(res, node.Val)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		vals = append(vals, res)
+		queue = queue[size:]
+		res = []int{}
+	}
+	return vals
+}
+
+// @lc code=end
+
+func levelOrder1(root *TreeNode) [][]int {
 	ret := [][]int{}
 	if root == nil {
 		return ret
@@ -30,7 +88,7 @@ func levelOrder(root *TreeNode) [][]int {
 	return ret
 }
 
-func levelOrder1(root *TreeNode) (vals []int) {
+func levelOrder2(root *TreeNode) (vals []int) {
 	if root == nil {
 		return
 	}
---
+++ leetcode-go/102.Binary-Tree-Level-Order/level_order_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/onwl007/leetcode-algo/structures"
+	"github.com/stretchr/testify/assert"
+)
+
+type Case struct {
+	in  *TreeNode
+	out [][]int
+}
+
+func TestLevelOrder(t *testing.T) {
+	qs := []Case{
+		{in: structures.Ints2TreeNode([]int{3, 9, 20, structures.NULL, structures.NULL, 15, 7}), out: [][]int{{3}, {9, 20}, {15, 7}}},
+		{in: structures.Ints2TreeNode([]int{1, 2, 3, 4, 5, 6, 7}), out: [][]int{{1}, {2, 3}, {4, 5, 6, 7}}},
+	}
+
+	ast := assert.New(t)
+	for _, v := range qs {
+		ast.Equal(v.out, levelOrder(v.in), "二叉树的层序遍历")
+		ast.Equal(v.out, levelOrder1(v.in), "二叉树的层序遍历")
+	}
+}
