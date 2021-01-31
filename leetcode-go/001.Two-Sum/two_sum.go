06700367851766dd59525c2d2afdb037f0df37b4
onwl007 <onwl007@126.com>
2021-01-31 21:58:01 +0800

    1 两数之和 增加单元测试

--- leetcode-go/001.Two-Sum/two_sum.go
+++ leetcode-go/001.Two-Sum/two_sum.go
@@ -1,12 +1,32 @@
 package main
 
+/*
+ * @lc app=leetcode.cn id=1 lang=golang
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
 func twoSum(nums []int, target int) []int {
 	m := make(map[int]int)
-	for i, v := range nums {
-		if j, ok := m[target-v]; ok {
+	for i := 0; i < len(nums); i++ {
+		if j, ok := m[target-nums[i]]; ok {
 			return []int{j, i}
 		}
-		m[v] = i
+		m[nums[i]] = i
+	}
+	return nil
+}
+
+// @lc code=end
+
+func twoSum1(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
 	}
 	return nil
 }
--- leetcode-go/001.Two-Sum/two_sum_test.go
+++ leetcode-go/001.Two-Sum/two_sum_test.go
@@ -1,57 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-type question1 struct {
-	para1
-	ans1
-}
-
-// para 是参数
-// one 代表第一个参数
-type para1 struct {
+type Case struct {
 	nums   []int
 	target int
-}
-
-// ans 是答案
-// one 代表第一个答案
-type ans1 struct {
-	one []int
+	out    []int
 }
 
 func TestTwoSum(t *testing.T) {
-	qs := []question1{
-		{
-			para1{[]int{3, 2, 4}, 6},
-			ans1{[]int{1, 2}},
-		},
-		{
-			para1{[]int{3, 2, 4}, 5},
-			ans1{[]int{0, 1}},
-		},
-		{
-			para1{[]int{0, 8, 7, 3, 3, 4, 2}, 11},
-			ans1{[]int{1, 3}},
-		},
-		{
-			para1{[]int{0, 1}, 1},
-			ans1{[]int{0, 1}},
-		},
-		{
-			para1{[]int{0, 3}, 5},
-			ans1{[]int{}},
-		},
+	qs := []Case{
+		{nums: []int{3, 2, 4}, target: 6, out: []int{1, 2}},
+		{nums: []int{3, 2, 4}, target: 5, out: []int{0, 1}},
+		{nums: []int{0, 8, 7, 3, 3, 4, 2}, target: 11, out: []int{1, 3}},
+		{nums: []int{0, 1}, target: 1, out: []int{0, 1}},
+		{nums: []int{0, 3}, target: 5, out: []int{}},
+		{nums: []int{3, 3}, target: 6, out: []int{0, 1}},
 	}
 
-	fmt.Printf("------------------------Leetcode Problem 1------------------------\n")
-
-	for _, q := range qs {
-		_, p := q.ans1, q.para1
-		fmt.Printf("【input】:%v      【output】:%v\n", p, twoSum(p.nums, p.target))
+	ast := assert.New(t)
+	for _, v := range qs {
+		ast.ElementsMatch(v.out, twoSum(v.nums, v.target), "两数之和")
+		ast.ElementsMatch(v.out, twoSum1(v.nums, v.target), "两数之和")
 	}
-	fmt.Printf("\n\n\n")
 }
