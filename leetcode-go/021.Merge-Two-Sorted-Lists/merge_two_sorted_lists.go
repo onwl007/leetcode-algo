959c68ef991c7e3194d57b8d552574a5ff0333f5
onwl007 <onwl007@126.com>
2021-05-07 23:25:41 +0800

    21 合并两个有序链表，递归解法

--- leetcode-go/021.Merge-Two-Sorted-Lists/merge_two_sorted_lists.go
+++ leetcode-go/021.Merge-Two-Sorted-Lists/merge_two_sorted_lists.go
@@ -48,3 +48,20 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 }
 
 // @lc code=end
+
+// 递归解法
+func mergeTwoLists1(l1, l2 *ListNode) *ListNode  {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists1(l1.Next, l2)
+		return l1
+	} else {
+		l2.Next = mergeTwoLists1(l1, l2.Next)
+		return l2
+	}
+}
--- leetcode-go/021.Merge-Two-Sorted-Lists/merge_two_sorted_lists_test.go
+++ leetcode-go/021.Merge-Two-Sorted-Lists/merge_two_sorted_lists_test.go
@@ -20,8 +20,18 @@ func TestMergeTwoLists(t *testing.T) {
 		{l1: structures.Ints2ListNode([]int{}), l2: structures.Ints2ListNode([]int{0}), out: []int{0}},
 	}
 
+	qs1 := []Case{
+		{l1: structures.Ints2ListNode([]int{1, 2, 4}), l2: structures.Ints2ListNode([]int{1, 3, 4}), out: []int{1, 1, 2, 3, 4, 4}},
+		{l1: structures.Ints2ListNode([]int{}), l2: structures.Ints2ListNode([]int{}), out: []int{}},
+		{l1: structures.Ints2ListNode([]int{}), l2: structures.Ints2ListNode([]int{0}), out: []int{0}},
+	} 
+
 	ast := assert.New(t)
 	for _, v := range qs {
 		ast.ElementsMatch(v.out, structures.ListNode2Ints(mergeTwoLists(v.l1, v.l2)), "合并两个有序链表")
+	}
+
+	for _, v := range qs1 {
+		ast.ElementsMatch(v.out, structures.ListNode2Ints(mergeTwoLists1(v.l1, v.l2)), "合并两个有序链表，递归解法")
 	}
 }
