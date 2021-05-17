43aabe071a2118c83eca58313eb762d98ebd1b75
onwl007 <onwl007@126.com>
2021-05-17 21:14:55 +0800

    148 排序链表增加自顶向下归并排序方法

--- leetcode-go/148.Sort-List/sort_list.go
+++ leetcode-go/148.Sort-List/sort_list.go
@@ -24,6 +24,54 @@ type ListNode = structures.ListNode
  * }
  */
 func sortList(head *ListNode) *ListNode {
+	return sort1(head, nil)
+}
+
+func sort1(left, right *ListNode) *ListNode {
+	if left == nil {
+		return left
+	}
+	if left.Next == right {
+		left.Next = nil
+		return left
+	}
+	slow, fast := left, left
+	for fast != right {
+		slow = slow.Next
+		fast = fast.Next
+		if fast != right {
+			fast = fast.Next
+		}
+	}
+	mid := slow
+	return mergeTwoList(sort1(left, mid), sort1(mid, right))
+}
+
+func mergeTwoList(l1, l2 *ListNode) *ListNode {
+	prehead := &ListNode{}
+	res := prehead
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			prehead.Next = l1
+			l1 = l1.Next
+		} else {
+			prehead.Next = l2
+			l2 = l2.Next
+		}
+		prehead = prehead.Next
+	}
+	if l1 != nil {
+		prehead.Next = l1
+	}
+	if l2 != nil {
+		prehead.Next = l2
+	}
+	return res.Next
+}
+
+// @lc code=end
+
+func sortList1(head *ListNode) *ListNode {
 	var arr []int
 	for head != nil {
 		arr = append(arr, head.Val)
@@ -39,5 +87,3 @@ func sortList(head *ListNode) *ListNode {
 	}
 	return res.Next
 }
-
-// @lc code=end
